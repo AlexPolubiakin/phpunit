dc3c0b6eee93f36d185b687c036fa4e8da9430d5
AlexPolubiakin <alpolubnev@gmail.com>
2018-07-26 21:06:37 +0300

    init

--- public/index.php
+++
@@ -1,4 +0,0 @@
-<?php 
-require_once '../app/boot.php';
-
-$ini = new Route;
---
+++ tests/ModelTests/ModelTest.php
@@ -0,0 +1,13 @@
+<?php
+use PHPUnit\Framework\TestCase;
+
+class SampleTest extends TestCase
+{
+
+    /**
+     * @test
+     */
+    public function test_get_list_func(){
+        
+    }
+}
